4086159e55a285935884a410b8412ca20418594e
cqlyj <luoyingjie0721@gmail.com>
2024-11-16 14:39:42 +0700

    add the userEmitEvent contract to be trigger by backend and then captured by chainlink node

--- smartcontract/src/UserEmitEvent.sol
+++ smartcontract/src/UserEmitEvent.sol
@@ -4,9 +4,16 @@ pragma solidity 0.8.26;
 import {Raffle} from "./Raffle.sol";
 
 contract UserEmitEvent {
-    event UserEvent(address indexed userWalletAddress, address indexed raffle);
+    event UserEvent(
+        address indexed userWalletAddress,
+        address indexed raffle,
+        uint256 indexed raffleNftTokenId
+    );
 
-    function interactWithCampaign(address raffle) public {
-        emit UserEvent(msg.sender, raffle);
+    function interactWithCampaign(
+        address raffle,
+        uint256 raffleNftTokenId
+    ) public {
+        emit UserEvent(msg.sender, raffle, raffleNftTokenId);
     }
 }
