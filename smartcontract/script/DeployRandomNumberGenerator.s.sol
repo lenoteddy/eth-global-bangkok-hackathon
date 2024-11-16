6675eedc3b2504fff168c21fe3b43be525dd13e9
cqlyj <luoyingjie0721@gmail.com>
2024-11-16 21:04:56 +0700

    upgrade VRF from v2 to v2.5

--- smartcontract/script/DeployRandomNumberGenerator.s.sol
+++ smartcontract/script/DeployRandomNumberGenerator.s.sol
@@ -6,19 +6,20 @@ import {RandomNumberGenerator} from "src/RandomNumberGenerator.sol";
 
 contract DeployRandomNumberGenerator is Script {
     address owner = 0xFB6a372F2F51a002b390D18693075157A459641F;
-    uint64 _subscriptionId = 9571;
-    address _vrfCoordinator = 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625;
-    bytes32 _keyHash =
-        0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
-    uint32 _callbackGasLimit = 2500000;
+    uint256 subscriptionId =
+        56003561226016405806153830093780721658933566768163361981188016113168550918782;
+    bytes32 gasLane =
+        0x816bedba8a50b294e5cbd47842baf240c2385f2eaf719edbd4f250a137a8c899;
+    uint32 callbackGasLimit = 2500000;
+    address vrfCoordinatorV2 = 0x343300b5d84D444B2ADc9116FEF1bED02BE49Cf2;
 
     function run() external returns (RandomNumberGenerator) {
         vm.startBroadcast(owner);
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator(
-            _subscriptionId,
-            _vrfCoordinator,
-            _keyHash,
-            _callbackGasLimit
+            subscriptionId,
+            gasLane,
+            callbackGasLimit,
+            vrfCoordinatorV2
         );
         vm.stopBroadcast();
         return (randomNumberGenerator);
