bc69d52525bc191f8535ee57671bab31eb0ec53a
cqlyj <luoyingjie0721@gmail.com>
2024-11-16 18:40:21 +0700

    modify the process of deploy raffle contract

--- smartcontract/script/DeployRaffle.s.sol
+++ smartcontract/script/DeployRaffle.s.sol
@@ -6,10 +6,11 @@ import {Raffle} from "src/Raffle.sol";
 
 contract DeployRaffle is Script {
     address owner = 0xFB6a372F2F51a002b390D18693075157A459641F;
+    address randomNumberGeneration = 0xCc67861552Bf06688eaB9A0F9bc3c43b906Cba2c;
 
     function run() external returns (Raffle) {
         vm.startBroadcast(owner);
-        Raffle raffle = new Raffle();
+        Raffle raffle = new Raffle(randomNumberGeneration);
         vm.stopBroadcast();
         return (raffle);
     }
